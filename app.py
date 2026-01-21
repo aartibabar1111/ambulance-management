0bbb428ca8506fcce09d5fb4f5ea249ec91bbebc
aarti <baarti23cs@student.mes.ac.in>
2026-01-21 16:57:32 +0530

    Update MySQL connection for Railway (host, port, SSL)

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ def get_db():
         password=os.getenv("MYSQLPASSWORD"),
         database=os.getenv("MYSQLDATABASE"),
         port=int(os.getenv("MYSQLPORT", 3306)),
+        ssl_disabled=False,
         autocommit=True
     )
 
